2fc9b5a7ec21aba08abb2f6722f80aa62ab4ec81
Rishabh <rishabhkachhot1409@gmail.com>
2026-06-23 16:32:22 +0530

    Use MongoDB Atlas connection string

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 
 dotenv.config()
 
-const url = 'mongodb://localhost:27017';
+const url = process.env.MONGO_URI;
 const client = new MongoClient(url);
 
 const dbName = 'vaultify'
